ee277e819128740135987ce38a9cca57cad04fea
Wesley-weg <145464683+Wesley-weg@users.noreply.github.com>
2024-10-01 18:20:32 +0200

    V0.6
    1ste pagina nav en responsive update

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,16 @@
 console.log("it works");
 let menuknop = document.querySelector("#hamburger")
 let balk = document.querySelector("#menu")
+let xknop = document.querySelector("#x")
 
     menuknop.addEventListener("click",toonmenu)
+    xknop.addEventListener("click",verbergmenu)
 
     function toonmenu(){
         balk.classList.add("toonhamburger")
         console.log("it changed class")
+    }
+
+    function verbergmenu(){
+        balk.classList.remove("toonhamburger")
     }
