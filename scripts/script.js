e97c743887c43f5c24b85fad294e17c23f55a2b0
Wesley-weg <145464683+Wesley-weg@users.noreply.github.com>
2024-10-01 15:50:01 +0200

    update 0.05
    javascript addition

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
-console.log("hi");
+console.log("it works");
+let menuknop = document.querySelector("#hamburger")
+let balk = document.querySelector("#menu")
+
+    menuknop.addEventListener("click",toonmenu)
+
+    function toonmenu(){
+        balk.classList.add("toonhamburger")
+        console.log("it changed class")
+    }
