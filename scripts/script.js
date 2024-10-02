4e12af3c612e2e194bc037b76fcc3a408d03f851
Wesley-weg <145464683+Wesley-weg@users.noreply.github.com>
2024-10-02 16:16:07 +0200

    V1.3
    pokeball animation

--- scripts/script.js
+++ scripts/script.js
@@ -3,9 +3,13 @@ console.log("it works");
 let menuknop = document.querySelector("#hamburger")
 let balk = document.querySelector("#menu")
 let xknop = document.querySelector("#x")
+let pokeball = document.querySelector("#pokeball")
+let lijst = document.querySelector("#lijst")
 
     menuknop.addEventListener("click",toonmenu)
     xknop.addEventListener("click",verbergmenu)
+
+    pokeball.addEventListener("click",verander)
 
     function toonmenu(){
         balk.classList.add("toonhamburger")
@@ -13,4 +17,11 @@ let xknop = document.querySelector("#x")
 
     function verbergmenu(){
         balk.classList.remove("toonhamburger")
+    }
+
+    function verander(){
+        pokeball.src = ("images/pokeballopen.png")
+        lijst.style.animation = "verander 1s"
+        document.querySelector("#lazers").style.animation = "lazer 1s"
+        lijst.classList.add("appear")
     }
